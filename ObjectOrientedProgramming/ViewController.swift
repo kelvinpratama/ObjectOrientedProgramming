719fb86db27641d6e1fb0cf064c13873128861ed
Kelvin Hadi Pratama <kelvinhadipratama@gmail.com>
2019-05-15 09:24:23 +0700

    Initial Commit

---
+++ ObjectOrientedProgramming/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectOrientedProgramming
+//
+//  Created by Kelvin Hadi Pratama on 15/05/19.
+//  Copyright © 2019 Kelvin Hadi Pratama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
